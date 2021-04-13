ef82c73946df62fafe164a4d0dfd8b3f6f8de227
jonahbaer <25778692+jonahbaer@users.noreply.github.com>
2021-04-13 12:57:17 -0400

    Remove ace-jump-mode, replace with avy.

--- emacs/init.el
+++ emacs/init.el
@@ -71,7 +71,7 @@
   :config
   (sml/setup))
 
-(use-package dashboard 
+(use-package dashboard
   :ensure t
   :config
   (setq dashboard-banner-logo-title "Welcome to (GNU) Emacs: An extensible, customizable, free/libre text editor - and more."
@@ -130,14 +130,13 @@
   (scheme-mode . aggressive-indent-mode)
   (racket-mode . aggressive-indent-mode))
 
-(use-package ace-jump-mode
+(use-package avy
   :config
-  (autoload
-    'ace-jump-mode
-    "ace-jump-mode"
-    "emacs quick move minor mode"
-    t)
-  (define-key global-map (kbd "C-c SPC") 'ace-jump-mode))
+  (global-set-key (kbd "C-:") 'avy-goto-char)
+  (global-set-key (kbd "C-'") 'avy-goto-char-2)
+  (global-set-key (kbd "M-g f") 'avy-goto-line)
+  (global-set-key (kbd "M-g w") 'avy-goto-word-1)
+  (global-set-key (kbd "M-g e") 'avy-goto-word-0))
 
 (use-package nlinum-relative
   :hook (prog-mode . nlinum-relative-mode))
